23414a6c4157b7c1b7f1ac262505eac93129d443
TomBlain <pm19769@bristol.ac.uk>
2022-11-19 21:00:53 +0000

    added baseline to report and cleaned up lightgbm

--- Tom/Lightgbm final.py
+++ Tom/Lightgbm final.py
@@ -39,28 +39,54 @@ def evalerror(preds, dtrain):
     labels = dtrain.get_label()
     return 'gini', gini(labels, preds), True
 
+def dropmissingcol(pdData):
+    vars_to_drop = ['ps_car_03_cat', 'ps_car_05_cat']
+    pdData.drop(vars_to_drop, inplace=True, axis=1)
+    return pdData
 
-train = pd.read_csv("Dataset/train.csv", dtype = {'id':np.int32, 'target':np.int8})
-test = pd.read_csv("Dataset/test.csv", dtype={'id':np.int32})
-
-vars_to_drop = ['ps_car_03_cat', 'ps_car_05_cat']
-train.drop(vars_to_drop, inplace=True, axis=1)
-test.drop(vars_to_drop, inplace=True, axis=1)
-
-
-
-def missingvalues(data):
+def missingvalues(pdData):
     mean_imp = SimpleImputer(missing_values=-1, strategy='mean')
     mode_imp = SimpleImputer(missing_values=-1, strategy='most_frequent')
     features = ['ps_reg_03','ps_car_12','ps_car_14','ps_car_11']
     for i in features:
         if i == 'ps_car_11':
-            data[i] = mode_imp.fit_transform(data[[i]]).ravel()
+            pdData[i] = mode_imp.fit_transform(pdData[[i]]).ravel()
         else:
-            data[i] = mean_imp.fit_transform(data[[i]]).ravel()
+            pdData[i] = mean_imp.fit_transform(pdData[[i]]).ravel()
+    return pdData
 
-missingvalues(train)
-missingvalues(test)
+def encodecat(train, test):
+    cat_features = [col for col in train.columns if '_cat' in col]
+    for column in cat_features:
+        temp = pd.get_dummies(pd.Series(train[column]), prefix=column)
+        train = pd.concat([train, temp], axis=1)
+        train = train.drop([column], axis=1)
+
+    for column in cat_features:
+        temp = pd.get_dummies(pd.Series(test[column]), prefix=column)
+        test = pd.concat([test, temp], axis=1)
+        test = test.drop([column], axis=1)
+    return train, test
+
+def RescaleData(train, test):
+    scaler = StandardScaler()
+    scaler.fit_transform(train)
+    scaler.fit_transform(test)
+    return train, test
+
+def DropCalcCol(train, test):
+    col_to_drop = train.columns[train.columns.str.startswith('ps_calc_')]
+    train = train.drop(col_to_drop, axis=1)
+    test = test.drop(col_to_drop, axis=1)
+    return train, test
+
+train = pd.read_csv("Dataset/train.csv", dtype = {'id':np.int32, 'target':np.int8})
+test = pd.read_csv("Dataset/test.csv", dtype={'id':np.int32})
+
+train = dropmissingcol(train)
+train = missingvalues(train)
+test = dropmissingcol(test)
+test = missingvalues(test)
 
 y_train = train['target'].values
 train_id = train['id'].values
@@ -69,32 +95,14 @@ X = train.drop(['target', 'id'], axis=1)
 test_id = test['id']
 X_test = test.drop(['id'], axis=1)
 
-col_to_drop = X.columns[X.columns.str.startswith('ps_calc_')]
-X = X.drop(col_to_drop, axis=1)
-X_test = X_test.drop(col_to_drop, axis=1)
-
-cat_features = [col for col in X.columns if '_cat' in col]
-for column in cat_features:
-    temp = pd.get_dummies(pd.Series(X[column]), prefix=column)
-    X = pd.concat([X, temp], axis=1)
-    X = X.drop([column], axis=1)
-
-for column in cat_features:
-    temp = pd.get_dummies(pd.Series(X_test[column]), prefix=column)
-    X_test = pd.concat([X_test, temp], axis=1)
-    X_test = X_test.drop([column], axis=1)
-
+X, X_test = DropCalcCol(X, X_test)
+X, X_test = encodecat(X, X_test)
 X = pd.DataFrame(X)
 X_test = pd.DataFrame(X_test)
-
-
-scaler = StandardScaler()
-scaler.fit_transform(X)
-scaler.fit_transform(X_test)
-X.to_csv("Dataset/encoded.csv")
+X, X_test = RescaleData(X, X_test)
 
 '''
-Best hyperparameters:
+Best hyperparameters from grid search:
 {'subsample': 0.4, 'num_leaves': 5, 'min_child_weight': 15, 'max_depth': 3, 'drop_rate': 0.3}
 '''
 
@@ -102,12 +110,6 @@ OPTIMIZE_ROUNDS = False
 LEARNING_RATE = 0.07
 EARLY_STOPPING_ROUNDS = 50
 
-
-'''
-model = lgbm.LGBMClassifier(
-    subsample = 0.4, num_leaves=5, min_child_weight=15, max_depth=3, drop_rate=0.3,
-    learning_rate = LEARNING_RATE, n_estimators = 600, objective='binary')
-'''
 min_data_in_leaf = 2000
 feature_fraction = 0.6
 num_boost_round = 10000
@@ -138,9 +140,8 @@ fold_scores = []
 
 cv_train = np.zeros(len(y_train))
 cv_pred = np.zeros(len(X_test))
-print(len(cv_pred))
-start_time = timer(None)
 
+start_time = timer(None)
 iterations = 1
 for seed in range(iterations):
     timer(start_time)
